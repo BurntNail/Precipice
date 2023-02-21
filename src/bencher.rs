9f1c6a61ae5859db716e970102c0f4dfca69810a
Jack Maguire <github.29x6f@simplelogin.com>
2023-02-21 14:40:10 +0000

    revert to pre-mt

--- src/bencher.rs
+++ src/bencher.rs
@@ -1,3 +1,4 @@
+
 use itertools::Itertools;
 use std::{
     env::current_dir,
@@ -14,8 +15,9 @@ pub struct Builder {
     cli_args: Vec<String>,
     runs: Option<usize>,
     stop_channel: Option<Receiver<()>>,
-    is_mt: Option<bool>,
 }
+
+const CHUNK_SIZE: usize = 25;
 
 impl Builder {
     pub const fn new() -> Self {
@@ -24,7 +26,6 @@ impl Builder {
             cli_args: vec![],
             runs: None,
             stop_channel: None,
-            is_mt: None,
         }
     }
 
@@ -55,82 +56,30 @@ impl Builder {
         self
     }
 
-    pub fn is_mt(mut self, is_mt: bool) -> Self {
-        self.is_mt = Some(is_mt);
-        self
-    }
-
     ///Panics if elements are not present
     pub fn start(self) -> (JoinHandle<io::Result<()>>, Receiver<Duration>) {
         let runs = self.runs.unwrap();
         let binary = self.binary.unwrap();
         let cli_args = self.cli_args;
         let stop_recv = self.stop_channel.unwrap();
-        let mt = self.is_mt.unwrap();
-
-        let chunk_size = if mt {
-            num_cpus::get() - 1
-        } else {
-            (runs / 10).max(25)
-        }
-        .max(1);
 
         let (duration_sender, duration_receiver) = channel();
         let handle = std::thread::spawn(move || {
-            let mut command = Command::new(binary.clone());
-            command.args(cli_args.clone());
+            let mut command = Command::new(binary);
+            command.args(cli_args);
             if let Ok(cd) = current_dir() {
                 command.current_dir(cd);
             }
 
-            let mut handles: Vec<JoinHandle<io::Result<()>>> = vec![];
-
-            let mut start;
-            for _chunk in (0..runs).into_iter().collect_vec().chunks(chunk_size) {
+            let mut start = Instant::now();
+            for _chunk in (0..runs).into_iter().collect_vec().chunks(CHUNK_SIZE) {
                 if stop_recv.try_recv().is_err() {
-                    for _ in 0..chunk_size {
+                    for _ in 0..CHUNK_SIZE {
+                        let _output = command.status()?;
+                        duration_sender
+                            .send(start.elapsed())
+                            .expect("Error sending result");
                         start = Instant::now();
-                        if mt {
-                            let new_sender = duration_sender.clone();
-                            let binary = binary.clone();
-                            let cli_args = cli_args.clone();
-                            handles.push(std::thread::spawn(move || {
-                                let mut command = Command::new(binary);
-                                command.args(cli_args);
-                                if let Ok(cd) = current_dir() {
-                                    command.current_dir(cd);
-                                }
-
-                                let start = Instant::now();
-                                let _output = command.status()?;
-                                new_sender
-                                    .send(start.elapsed())
-                                    .expect("Error sending result");
-
-                                Ok(())
-                            }));
-                        } else {
-                            let _output = command.status()?;
-                            duration_sender
-                                .send(start.elapsed())
-                                .expect("Error sending result");
-                        }
-                    }
-
-                    //TODO: threadpool for handles, maybe add rayon as a dep?
-                    println!("Handles has {}", handles.len());
-
-                    while !handles.is_empty() {
-                        let mut no_removed = 0;
-                        for i in 0..handles.len() {
-                            if handles[i - no_removed].is_finished() {
-                                handles
-                                    .remove(i - no_removed)
-                                    .join()
-                                    .expect("error joining handle")?;
-                                no_removed += 1;
-                            }
-                        }
                     }
                 } else {
                     break;
